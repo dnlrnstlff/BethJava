20d87a3e4cb166b27fdf69e10ffb26763a4c929f
Daniel-Ernest Luff <del@danielernestluff.co.uk>
2014-12-26 07:13:41 -0500

    Comments added for eou.

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
+/*	Beth Parse Cloud Code
+	Daniel-Ernest Luff
+
+A simple way to easily delete entries on Parse Core using Cloud Code. Just schedule through Parse.
+
+*/
+
+//Name the parse job
 Parse.Cloud.job("deleteMessages", function(request, status) {
 
   Parse.Cloud.useMasterKey();
 
+	//Current time
     var ts = Math.round(new Date().getTime() / 1000);
+    //
+    //Time difference variable. Change this for the altered time difference.
     var tsYesterday = ts - (7 * 24 * 3600);
     var dateYesterday = new Date(tsYesterday*1000);
 
+	//Parse table query. (Case-Sen duh)
     var query = new Parse.Query("Messages");
 
+	//Parse row query. (Camel-case duh)
     query.lessThan("updatedAt", dateYesterday);
 
     query.find({
